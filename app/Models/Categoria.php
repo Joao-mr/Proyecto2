22fb2f1c77f21b5d124879a8dbcb455d6e05be38
Miquel Manzano <mmanz2606@gmail.com>
2026-02-13 18:53:48 +0100

    Revert "CRUD inicial API Categorias"
    This reverts commit ff371ceb1a520f58a6f665951950e1afd585f2ea.

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -6,11 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    protected $fillable = ['nombre'];
-
     //
-    public function salas()
-    {
-        return $this->belongsToMany(Sala::class, 'sala_categorias', 'id_categoria', 'id_sala');
-    }
 }
