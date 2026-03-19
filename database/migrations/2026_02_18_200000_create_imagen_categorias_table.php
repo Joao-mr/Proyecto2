48d393cea709f83f8b2e80e3c4ced08ab96e1b3e
Miquel Manzano <mmanz2606@gmail.com>
2026-03-19 16:25:59 +0100

    migration imagen-categoria

---
+++ database/migrations/2026_02_18_200000_create_imagen_categorias_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('imagen_categorias', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_imagen');
+            $table->unsignedBigInteger('id_categoria');
+
+            $table->primary(['id_imagen', 'id_categoria']);
+
+            $table->foreign('id_imagen')
+                ->references('id')
+                ->on('media')
+                ->onDelete('cascade');
+
+            $table->foreign('id_categoria')
+                ->references('id')
+                ->on('categorias')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('imagen_categorias');
+    }
+};
