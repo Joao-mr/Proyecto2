16a5145402e28331f5bd6dae6fe7fdefaa947dba
Joao Meneses <joaomenesesfp@ibf.cat>
2026-04-07 17:51:01 +0200

    Navbar y banner acabado falta retocar

--- resources/js/layouts/GuestLayout.vue
+++ resources/js/layouts/GuestLayout.vue
@@ -1,12 +1,8 @@
 <template>
     <div class="min-h-screen flex flex-col">
-        <HomeNavbar/>
         <main class="flex-grow">
             <router-view></router-view>
         </main>
     </div>
 </template>
-<script setup>
-import HomeNavbar from './HomeNavbar.vue'
 
-</script>
--- resources/js/layouts/HomeNavbar.vue
+++ resources/js/layouts/HomeNavbar.vue
@@ -1,8 +1,9 @@
-<!-- filepath: c:\xampp\htdocs\Proyecto2\Laravel-VUE-API-Base-Clase\resources\js\layouts\HomeNavbar.vue -->
 <template>
   <header class="home-header">
     <div class="container-home home-header__inner">
-      <RouterLink to="/" class="home-logo">WHAT<span>IZIT</span></RouterLink>
+      <RouterLink to="/" class="home-logo" aria-label="Whatizit - Inicio">
+        <img src="/images/logowhatizit.svg" alt="Whatizit" class="home-logo-img" />
+      </RouterLink>
 
       <nav class="home-nav">
         <a href="#" class="home-nav-item">Juegos</a>
