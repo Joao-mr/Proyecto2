399189eaaa065d57e80f4a47f23447a83ce5ef48
Miquel Manzano <mmanz2606@gmail.com>
2026-04-16 16:28:24 +0200

    perfeccionando el desarrollo hasta ahora

--- app/Models/Imagen.php
+++ app/Models/Imagen.php
@@ -19,6 +19,11 @@ class Imagen extends Model implements HasMedia
         'respuesta_correcta',
     ];
 
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class, 'categoria_id');
+    }
+
     public function categorias()
     {
         return $this->belongsToMany(Categoria::class, 'imagen_categoria', 'id_imagen', 'id_categoria');
@@ -27,11 +32,6 @@ class Imagen extends Model implements HasMedia
     public function partidas()
     {
         return $this->belongsToMany(Partida::class, 'partida_imagen', 'id_imagen', 'id_partida')->withPivot('ronda');
-    }
-
-    public function respuestas()
-    {
-        return $this->hasMany(Respuesta::class, 'id_imagen');
     }
 
     //se crea una carpeta "logica" que se llama imagenes
--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\Api\PartidaController;
 use App\Http\Controllers\Api\PartidaImagenController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\ProfileController;
-use App\Http\Controllers\Api\RespuestaController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\SalaController;
 use App\Http\Controllers\Api\SalaCategoriaController;
@@ -51,9 +50,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('imagenes/{imagen}/upload', [ImagenController::class, 'uploadImage'])->name('imagenes.upload');
     Route::get('imagenes/{imagen}/media-info', [ImagenController::class, 'getMediaInfo'])->name('imagenes.media-info');
     Route::get('imagenes/{imagen}/all-media', [ImagenController::class, 'getAllMedia'])->name('imagenes.all-media');
-
-    //respuestas
-    Route::apiResource('respuestas', RespuestaController::class);
 
     //usuario-partida
     Route::get('usuario-partidas', [UsuarioPartidaController::class, 'index']);
